f40b27092446a0a9d153e03f7323f1f807dbb202
abhi <mkabhijith149@gmail.com>
2024-01-17 20:35:54 +0530

    next commit

---
+++ phpMyAdmin/tmp/twig/cf/cf529867206874ecf6aa4ddfe533391cffd3ed46acd0117d72dfdd30fe2d26a3.php
@@ -0,0 +1,67 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* display/results/value_display.twig */
+class __TwigTemplate_70efcd7d70bd386b8de7bc5127a67fe7fb3bf2e0ca429c6aa1b55042c16b9f5c extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<td class=\"left ";
+        echo twig_escape_filter($this->env, ($context["class"] ?? null), "html", null, true);
+        echo ((($context["condition_field"] ?? null)) ? (" condition") : (""));
+        echo "\">";
+        // line 2
+        echo ($context["value"] ?? null);
+        // line 3
+        echo "</td>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "display/results/value_display.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  44 => 3,  42 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "display/results/value_display.twig", "C:\\xampp\\phpMyAdmin\\templates\\display\\results\\value_display.twig");
+    }
+}
